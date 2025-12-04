c9968aa855b14159ffe4250f4e36bbdcc142c950
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-12-04 17:09:22 +0530

    Update cie3.py

--- cie3.py
+++ cie3.py
@@ -7,7 +7,7 @@ else:
     name = "shweta"
     marks = [45, 50, 65]
 
-average = sum(marks) / len(marks)
+average=sum(marks)/len(marks)
 
 print("student name:", name)
 print("Internal test marks:",marks)
