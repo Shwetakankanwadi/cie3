0dfe8b28a8a3f0c321f257432f7d2c99e5ef5b08
Shwetakankanwadi <shwetakankanwadi878@gmail.com>
2025-12-04 17:06:31 +0530

    Update cie3.py

--- cie3.py
+++ cie3.py
@@ -4,7 +4,7 @@ if len(sys.argv) >= 3:
     name = sys.argv[1]
     marks = [float(m) for m in sys.argv[2]]  
 else:
-    name = "xyz"
+    name = "shweta"
     marks = [45, 50, 65]
 
 average = sum(marks) / len(marks)
