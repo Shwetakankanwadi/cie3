867d763d5402a797f16de065a6b0a3bfafe653e2
Shwetakankanwadi <Shwetakankanwadi878@gmail.com>
2025-12-04 16:19:15 +0530

    save

---
+++ cie3.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) >=3:
+    name="Shweta"
+    marks=[float(m)for m in sys.argv[2]]
+    
+else:
+    name="Shweta"
+    marks=[45,50,65]
+    
+    average=sum(marks)/len(marks)
+    
+    print("student name:",name)
+    print("Internal test marks:",marks)
+    print("average internal score:",round(average,2))
+  
